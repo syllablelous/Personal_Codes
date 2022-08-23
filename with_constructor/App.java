5901a5aceb2e86b90b0cb8dad79f09895ef65801
Russel Jerome Rapi <111546620+syllablelous@users.noreply.github.com>
2022-08-23 11:33:16 +0800

    Create App.java

---
+++ with_constructor/App.java
@@ -0,0 +1,25 @@
+public class App {
+    public static void main(String[] args) {
+
+        // Creates a new object named "Knight" from Character class
+        Character knight = new Character(10, 5, 0, "Chen");
+        // Calls the sayMyName method
+        knight.sayMyName();
+
+        // Creates a new object named "mage" from Character class
+        Character mage = new Character(0, 5, 10, "Akasha");
+        // Calls the sayMyName method
+        mage.sayMyName();
+
+        // Creates a new object named "mage" from Character class
+        Character thief = new Character(5, 10, 0, "Gondar");
+        // Calls the sayMyName method
+        thief.sayMyName();
+
+        Character healer = new Character(2, 7, 10, "Arius");
+        healer.sayMyName();
+        
+        Character archer = new Character(8, 6, 5, "Lumos");
+        archer.sayMyName();
+    }
+}
